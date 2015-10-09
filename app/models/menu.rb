0782223dcf98640d93ecd20d11ba9fbb7d04c5b6
Gaurav Kumar <gaurav.kumar@chronus.com>
2015-10-10 01:14:14 +0530

    updated menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,15 +1,12 @@
 class Menu < ActiveRecord::Base
   attr_accessible :name, :price, :category, :location_id, :menu_type
-  validates :name, :price, :presence => true
-  validates :menu_type, :presence => true, inclusion: { in: Type.all }
-  validates :category, :presence => true, inclusion: { in: Category.all }
-
+  
   module Type
   	RICE ='rice'
   	GRAVY ='gravy'
   	DRINk ='drink'
   	PRATHA ='paratha'
-  	
+
   	def self.all
   	  [RICE, GRAVY, DRINK, PRATHA]
   	end
@@ -23,4 +20,9 @@ class Menu < ActiveRecord::Base
   	  [VEG, NON_VEG]
   	end
   end 
+
+  validates :name, :price, :presence => true
+  validates :menu_type, :presence => true, inclusion: { in: Type.all }
+  validates :category, :presence => true, inclusion: { in: Category.all }
+  
 end
