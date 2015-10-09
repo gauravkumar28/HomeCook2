112c51feee7ac4867a0286a28b584b8138e48317
Gaurav Kumar <gaurav.kumar@chronus.com>
2015-10-10 01:29:55 +0530

    update user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ActiveRecord::Base
   # attr_accessible :title, :body
   has_many :orders
   has_one :shipping_address
+  validates :email, :password, :phone, :name, :presence => true
 end
