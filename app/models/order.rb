e9c0653cc581b1837ee6df8157a2bd448420ba70
Gaurav Kumar <gaurav.kumar@chronus.com>
2015-10-31 15:08:46 +0530

    updated order summary

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,4 +15,14 @@ class Order < ActiveRecord::Base
 
   DELIVERY_CHARGE = 20
   ADMIN_CONTACT_NUMBER = 9986895741
+
+  def generate_summery(cart)
+    item_names = cart.cart_items.collect(&:item).collect(&:name)
+    item_quantity = cart.cart_items.collect(&:quantity)
+    summery = []
+    item_names.each_with_index do |name, index|
+      summery << "#{name} - #{item_quantity[index]}"
+    end
+    summery.join(", ")
+  end
 end
