b3a6e241604a22e69ad1cceba49c7d21504536e9
Gaurav Kumar3 <gaurav.kumar3@C02QD5TFG8WP.local>
2015-11-22 23:37:10 +0530

    fixed session issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,12 @@ class ApplicationController < ActionController::Base
     #session.delete(:cart_id)
     cart_id = session[:cart_id]
     if session[:cart_id]
-      @cart = Cart.find(cart_id)
-      @cart ||=Cart.create
+      begin
+        @cart = Cart.find(cart_id)
+      rescue => e
+
+        @cart =Cart.create
+      end
     else
        @cart = Cart.create
     end
