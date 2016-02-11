f7445954387128f4eb84fe7be21a4df778ff2119
Gaurav Kumar3 <gaurav.kumar3@C02QD5TFG8WP.local>
2016-02-12 01:49:53 +0530

    fixed bug

--- app/admin/menu.rb
+++ app/admin/menu.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Menu do
+  form do |f|
+    f.inputs "Menu Details" do
+      f.input :name
+      f.input :price
+      f.input :category
+      f.input :location_id
+      f.input :menu_type
+    end
+    f.actions
+  end
 
 end
