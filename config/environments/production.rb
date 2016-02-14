9d79c16c5f310776795afaf474b11ca555aa09ff
Gaurav Kumar <gaurav.kumar@chronus.com>
2016-02-15 04:28:13 +0530

    fixed all ui issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ HomeCook::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
   config.assets.precompile += ['active_admin.css', 'active_admin.js', 'active_admin/print.css']
+  config.assets.precompile += %w( public.js public.css  )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
